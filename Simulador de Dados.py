17ee57120dd069c2acf16363abf801ef5398760f
hamsety <felipe.felgueiras1@gmail.com>
2022-09-27 17:51:23 -0300

    Finalizando projeto dos dados

--- Simulador de Dados.py
+++ Simulador de Dados.py
@@ -3,9 +3,20 @@
 # Verificar a ordem do maior para o menor do sorteio dos dados
 
 from random import randint
+from time import  sleep
+from operator import itemgetter
 jogo = { 'Jogador 1': randint(1, 6),
          'Jogador 2': randint(1, 6),
          'Jogador 3': randint(1, 6),
          'Jogador 4': randint(1, 6)}
-
-print(jogo)
+ranking = list()
+print('Valores sorteados:')
+for k, v in jogo.items():
+    print(f'{k} tirou {v} no dado')
+    sleep(1)
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+print('¨¨' * 30)
+print('-- Ranking dos jogadores --')
+for i, v in enumerate(ranking):
+    print(f'{i+1}º lugar: {v[0]} tirando {v[1]} no dado.')
+    sleep(1)
